db5210689e05cca0458196eba3351bc7ed97f0be
Gustavo Cristian <diamonddyaghas@gmail.com>
2023-02-25 21:39:56 -0300

    car methods moved to Car class | read is possible now (CRUD)

--- src/com/hashpack/Car.java
+++ src/com/hashpack/Car.java
@@ -1,27 +1,128 @@
 package com.hashpack;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Scanner;
+
 class Car {
 
     private int id;
     private String brand;
     private String color;
     private int year;
-    private String[] brands = new String[]{"bmw", "chevrolet", "ford", "honda", "hyundai", "porsche", "toyota"};
+    private static String[] brands = new String[]{"bmw", "chevrolet", "ford", "honda", "hyundai", "porsche", "toyota"};
 
-    public Car(int id, String b, String c, int y) {
-        setId(id);
-        setBrand(b);
-        setColor(c);
-        setYear(y);
+    public Car() {
+
     }
 
-    public String getCarParameters() {
-        String carInstance = "ID: " + getId() + "\n" +
-                "Brand:" + getBrand() + "\n" +
-                "Color: " + getColor() + "\n" +
-                "Model year: " + getYear() + "\n" +
-                "-------------------------------";
-        return carInstance;
+    public static boolean verifyBrand(String b) {
+        for(String e : brands) {
+            if(e.equals(b)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean verifyYear(int y) {
+        if(y > 1886 && y < 2023) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void createCarInstance(int id, Connection myConn) {
+        Scanner scanner = new Scanner(System.in);
+        id = id + 1;
+        System.out.print("Car brand: ");
+        String brand = scanner.nextLine();
+        System.out.print("Car color: ");
+        String color = scanner.nextLine();
+        System.out.print("Car year: ");
+        int model_year = scanner.nextInt();
+
+        //verify if car brand is valid
+        if(verifyBrand(brand)) {
+            //verify if car manufacture year is valid
+            if(verifyYear(model_year)) {
+                System.out.println(id);
+                System.out.println(brand);
+                System.out.println(color);
+                System.out.println(model_year);
+
+                try {
+                    PreparedStatement pstmt = myConn.prepareStatement(
+                            "INSERT INTO car(brand, color, model_year) VALUE(?, ?, ?)");
+                    pstmt.setString(1, brand);
+                    pstmt.setString(2, color);
+                    pstmt.setInt(3, model_year);
+                    pstmt.executeUpdate();
+                } catch (Exception IllegalArgumentException) {
+                    System.out.println("Adding the car was not possible");
+                }
+            } else {
+                System.out.println("Invalid year");
+            }
+        } else {
+            System.out.println("Invalid brand");
+        }
+    }
+
+    public static void listCars(Connection conn){
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(
+                    "SELECT car_id, color, model_year, brand FROM car");
+            ResultSet rs = pstmt.executeQuery();
+            while(rs.next()) {
+                int id = rs.getInt(1);
+                String color = rs.getString(2);
+                String modelYear = rs.getString(3);
+                String brand = rs.getString(4);
+                System.out.println("ID: " + id + "\n" +
+                        "Brand:" + brand + "\n" +
+                        "Color: " + color + "\n" +
+                        "Model year: " + modelYear + "\n" +
+                        "-------------------------------");
+            }
+            rs.close();
+            pstmt.close();
+        } catch (Exception IllegalArgumentException) {
+            System.out.println("Listing cars was not possible");
+        }
+    }
+
+    public static void searchCar(Connection conn) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("insert car ID: ");
+            int id = scanner.nextInt();
+            PreparedStatement pstmt = conn.prepareStatement(
+                    "SELECT car_id, color, model_year, brand FROM car WHERE car_id = ?");
+            pstmt.setInt(1, id);
+            ResultSet rs = pstmt.executeQuery();
+            while(rs.next()) {
+                id = rs.getInt(1);
+                String color = rs.getString(2);
+                String modelYear = rs.getString(3);
+                String brand = rs.getString(4);
+                System.out.println("ID: " + id + "\n" +
+                        "Brand:" + brand + "\n" +
+                        "Color: " + color + "\n" +
+                        "Model year: " + modelYear + "\n" +
+                        "-------------------------------");
+            }
+            rs.close();
+            pstmt.close();
+        } catch (Exception IllegalArgumentException) {
+            System.out.println("Searching for the car was not possible");
+        }
+    }
+
+    public static void deleteCar(int id) {
+
     }
 
     public String getBrand() {
--- src/com/hashpack/Main.java
+++ src/com/hashpack/Main.java
@@ -1,49 +1,13 @@
 package com.hashpack;
 
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-    static ArrayList<Car> cars = new ArrayList<>();
+    static Car car = new Car() {
 
-    public static void createCarInstance(int id, Connection myConn) {
-        Scanner scanner = new Scanner(System.in);
-        id = id + 1;
-        System.out.print("Car brand: ");
-        String brand = scanner.nextLine();
-        System.out.print("Car color: ");
-        String color = scanner.nextLine();
-        System.out.print("Car year: ");
-        int model_year = scanner.nextInt();
-
-        System.out.println(id);
-        System.out.println(brand);
-        System.out.println(color);
-        System.out.println(model_year);
-
-        try {
-            Car car = new Car(id++, brand, color, model_year);
-            PreparedStatement pstmt = myConn.prepareStatement(
-                    "INSERT INTO car(brand, color, model_year) VALUE(?, ?, ?)");
-            pstmt.setString(1, brand);
-            pstmt.setString(2, color);
-            pstmt.setInt(3, model_year);
-            pstmt.executeUpdate();
-        } catch (Exception IllegalArgumentException) {
-            System.out.println("Adding the car was not possible");
-        }
-    }
-
-    public static void listCars(Statement stmt) throws SQLException {
-        ResultSet rs = stmt.executeQuery("select * from car");
-        int j = 0;
-        while(rs.next()) {
-            System.out.println(cars.get(j).getCarParameters());
-            j++;
-        }
-    }
+    };
 
     public static void main(String[] args) {
         try {
@@ -51,51 +15,37 @@ public class Main {
             Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/car_database",
                     "root", "");
             Statement myStmt = myConn.createStatement();
-            ResultSet myRs = myStmt.executeQuery("select * from car");
-
-            int i = 0;
             int id = 0;
-            while(myRs.next()) {
-                //instantiate car parameters
-                id = myRs.getInt("Car_id");
-                String brand = myRs.getString("brand");
-                String color = myRs.getString("color");
-                int modelYear = myRs.getInt("model_year");
-
-                //create a car object
-                Car car = new Car(id, brand, color, modelYear);
-                //add car object to cars arrayList
-                cars.add(car);
-                System.out.println(cars.get(i).getCarParameters());
-                i++;
-            }
-
+            //input initialization, value has to be different from "finish"
             String input = "new";
-
             while(!input.equals("finish")) {
-                System.out.print("commands: " +
-                        "\n 'new' - add new element " +
-                        "\n 'list' - list elements " +
-                        "\n'finish' - finish application \n\n"
+                System.out.print("""
+                        commands:\s
+                        'new' - add new car\s
+                        'list' - list cars\s
+                        'finish' - finish application\s
+                        'search' - search for a specific car
+
+                        """
                 );
                 Scanner scan = new Scanner(System.in);
                 input = scan.nextLine();
-
-                //create new element
-                if (input.equals("new")) {
-                    createCarInstance(id, myConn);
-                }
-
-                //list elements
-                if(input.equals("list")) {
-                    listCars(myStmt);
+                switch(input) {
+                    case "new":
+                        car.createCarInstance(id, myConn);
+                        break;
+                    case "list":
+                        car.listCars(myConn);
+                        break;
+                    case "search":
+                        car.searchCar(myConn);
+                        break;
+                    default:
+                        System.out.println("invalid command");
                 }
             }
-
-            myRs.close();
             myStmt.close();
             myConn.close();
-
         } catch(Exception exc) {
 
         }
