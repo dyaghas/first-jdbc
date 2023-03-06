1aee27cc55b4cf720870213f99d7002c473e9443
Gustavo Cristian <diamonddyaghas@gmail.com>
2023-03-06 15:26:41 -0300

    Owner class added

--- src/com/hashpack/Main.java
+++ src/com/hashpack/Main.java
@@ -10,41 +10,51 @@ public class Main {
         try {
             //Connect to database
             Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/car_database",
-                    "root", "");
+                    "root", "phpmyadmin@777@Dyaghas");
             Statement myStmt = myConn.createStatement();
 
-            int id = 0;
             //input initialization, value has to be different from "finish"
             String input = "new";
             while(!input.equals("finish")) {
                 System.out.print("""
                         commands:\s
-                        'new' - add new car\s
-                        'delete' - delete a car\s
-                        'update' - update car information\s
-                        'list' - list cars\s
-                        'search' - search for a specific car\s
+                        owner:
+                            'register-owner' - register new owner\s
+                        car:
+                            'new-car' - add new car\s
+                            'delete-car' - delete a car\s
+                            'update-car' - update car information\s
+                            'list-cars' - list cars\s
+                            'search-car' - search for a specific car\s
+                            
                         'finish' - finish application\s
                                                 
                         """
                 );
 
-                //select which action will be done
                 Scanner scan = new Scanner(System.in);
+
+                //select which action will be done
                 input = scan.nextLine();
                 switch(input) {
-                    case "new":
-                        Car.createCarInstance(id, myConn);
+                    case "register-owner":
+                        Owner.registerOwner(myConn);
                         break;
-                    case "list":
+                    case "list-owners":
+                        Owner.listOwners(myConn);
+                        break;
+                    case "new-car":
+                        Car.createCarInstance(myConn);
+                        break;
+                    case "list-cars":
                         Car.listCars(myConn);
                         break;
-                    case "search":
+                    case "search-car":
                         Car.searchCar(myConn, Car.getCarId());
                         break;
-                    case "update":
+                    case "update-car":
                         Car.updateCar(myConn, Car.getCarId());
-                    case "delete":
+                    case "delete-car":
                         Car.deleteCar(myConn, Car.getCarId());
                         break;
                     default:
---
+++ src/com/hashpack/Owner.java
@@ -0,0 +1,55 @@
+package com.hashpack;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Scanner;
+
+public class Owner {
+
+
+    public static void registerOwner(Connection conn) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Owner first name: ");
+        String first_name = scanner.nextLine();
+        System.out.print("Owner second name: ");
+        String second_name = scanner.nextLine();
+        System.out.print("Age: ");
+        int age = scanner.nextInt();
+
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(
+                    "INSERT INTO owner(first_name, second_name, age) VALUE(?, ?, ?)");
+            pstmt.setString(1, first_name);
+            pstmt.setString(2, second_name);
+            pstmt.setInt(3, age);
+            pstmt.executeUpdate();
+            System.out.println("Owner successfully added to database");
+        } catch (Exception IllegalArgumentException) {
+            System.out.println("Registering owner was not possible");
+        }
+    }
+
+    public static void listOwners(Connection conn) {
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(
+                    "SELECT owner_id, first_name, second_name, age FROM owner");
+            ResultSet rs = pstmt.executeQuery();
+            while(rs.next()) {
+                int id = rs.getInt(1);
+                String first_name = rs.getString(2);
+                String second_name = rs.getString(3);
+                String age = rs.getString(4);
+                System.out.println("ID: " + id + "\n" +
+                        "First name:" + first_name + "\n" +
+                        "Second name: " + second_name + "\n" +
+                        "Age: " + age + "\n" +
+                        "-------------------------------");
+            }
+            rs.close();
+            pstmt.close();
+        } catch (Exception IllegalArgumentException) {
+            System.out.println("Listing owners was not possible");
+        }
+    }
+}
